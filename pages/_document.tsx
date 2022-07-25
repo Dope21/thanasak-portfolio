2a53056496ce28e7ffa932e9804441240c248e21
Thanasak <bestzacap20@gmail.com>
2022-07-25 21:41:49 +0700

    set class theme to light

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head />
-        <body>
+        <body className="light">
           <Main />
           <NextScript />
         </body>
