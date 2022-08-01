8007cdcedc58e579abee8fd6bd3f86ae79a2372e
Thanasak <bestzacap20@gmail.com>
2022-08-01 17:47:54 +0700

    init commmit

---
+++ components/button.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import { AiOutlineGithub } from 'react-icons/ai'
+
+export const BlackBtn = ({ children, icon, href }) => {
+  return (
+    <Link href={href}>
+      <a className="bg-black text-white inline-flex items-center px-4 py-2 border border-black rounded-md gap-1 tracking-wide dark:text-black dark:bg-white dark:border-white">
+        {children}
+        {icon}
+      </a>
+    </Link>
+  )
+}
+
+export const OutlineBtn = ({ children, icon, href }) => {
+  return (
+    <Link href={href}>
+      <a className="text-zinc-500 inline-flex items-center px-4 py-2 border border-zinc-500 rounded-md gap-1 tracking-wide dark:text-black dark:bg-white dark:border-white">
+        {children}
+        {icon}
+      </a>
+    </Link>
+  )
+}
+
+export const GithubBtn = () => {
+  return (
+    <a
+      href="https://github.com/Dope21"
+      rel="noreferrer"
+      target="_blank"
+      className="bg-black text-white flex items-center px-4 py-2 border border-black rounded-md gap-1 tracking-wide"
+    >
+      GitHub
+      <AiOutlineGithub size={20} />
+    </a>
+  )
+}
