2317979d1ed18c3a3acdb9b34c61a6677942550e
Thanasak <bestzacap20@gmail.com>
2022-08-03 17:37:06 +0700

    create theme function

--- components/theme-button.tsx
+++ components/theme-button.tsx
@@ -1,27 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 
 import sunIcon from '../public/images/icons/sun.png'
 import moonIcon from '../public/images/icons/moon.png'
 
 const Theme = () => {
-  const [theme, setTheme] = useState(false)
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const currentTheme = localStorage.getItem('theme')
+      if (currentTheme) return currentTheme
+    } else return 'light'
+  })
+
+  useEffect(() => {
+    localStorage.theme = theme
+    const body = document.getElementsByTagName('body')[0]
+    body.setAttribute('class', localStorage.theme)
+  }, [theme])
+
   const handleTheme = () => {
-    setTheme(!theme)
+    setTheme(theme => (theme === 'light' ? 'dark' : 'light'))
   }
+
   return (
     <div
-      className="inline-flex relative items-center cursor-pointer select-none w-[70px] h-[38px] rounded-full bg-gradient-to-r from-blue-500 to-green-400"
+      className="theme-button-color inline-flex relative items-center cursor-pointer select-none w-[70px] h-[38px] rounded-full"
       onClick={handleTheme}
     >
-      <div
-        className={`absolute h-[34px] w-[34px] bg-white rounded-full left-[2px] top-[2px] transition-all flex items-center justify-center 
-        ${theme ? 'translate-x-[95%]' : ''}`}
-      >
+      <div className="absolute h-[34px] w-[34px] bg-white rounded-full top-[2px] left-[2px] transition-all flex items-center justify-center translate-x-0 dark:translate-x-[95%]">
         <Image
           width={16}
           height={16}
-          src={theme ? moonIcon : sunIcon}
+          src={theme === 'light' ? sunIcon : moonIcon}
           alt="theme-icon"
         />
       </div>
