81f63c5f0087416e53327025e8843198821cb08d
Thanasak <bestzacap20@gmail.com>
2022-08-03 17:38:23 +0700

    changed color class &  add padding left to Title

--- components/title.tsx
+++ components/title.tsx
@@ -1,7 +1,9 @@
 export const Heading = ({ children }) => {
-  return <h3 className="text-xl font-bold mb-4">{children}</h3>
+  return <h3 className="text-xl font-bold mb-4 main-text-color">{children}</h3>
 }
 
 export const Title = ({ children }) => {
-  return <h4 className="text-xl font-bold mb-4">{children}</h4>
+  return (
+    <h4 className="pl-7 text-xl font-bold mb-4 main-text-color">{children}</h4>
+  )
 }
