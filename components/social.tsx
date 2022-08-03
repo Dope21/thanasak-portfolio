c34c4b8521fb29ad5e778fd6f97c779380854df5
Thanasak <bestzacap20@gmail.com>
2022-08-03 17:36:31 +0700

    wrap a tag with div & changed to inline-flex

--- components/social.tsx
+++ components/social.tsx
@@ -1,14 +1,16 @@
 const Social = ({ children, href, icon }) => {
   return (
-    <a
-      href={href}
-      target="_blank"
-      rel="noreferrer"
-      className=" text-blue-500 flex items-center gap-5 rounded-md transition-all hover:bg-blue-200 p-2 dark:text-pink-500 dark:hover:bg-pink-200"
-    >
-      {icon}
-      {children}
-    </a>
+    <div>
+      <a
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+        className="social-color px-6 py-2 inline-flex items-center gap-3 rounded-md transition-all"
+      >
+        {icon}
+        {children}
+      </a>
+    </div>
   )
 }
 
