2066d580cde20b94e67dfa86c4e240720f6e274d
Thanasak <bestzacap20@gmail.com>
2022-08-04 09:52:56 +0700

    init commit

---
+++ components/projects-item.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Section from './section'
+import { BlackBtn } from './button'
+import { AiOutlineRight } from 'react-icons/ai'
+
+const Project = ({ src, alt, title, desc, href }) => {
+  return (
+    <Section>
+      <div className="flex flex-col sm:flex-row items-center gap-4 md:gap-10 relative">
+        <div className="max-w-[275px] max-h-[175px]">
+          <Image
+            src={src}
+            alt={alt}
+            placeholder="blur"
+            className="rounded-md flex"
+          />
+        </div>
+
+        <div>
+          <h4 className="text-xl main-text-color font-semibold mb-[10px]">
+            {title}
+          </h4>
+          <p className="sub-text-color mb-4">{desc}</p>
+          <BlackBtn icon={<AiOutlineRight size={18} />} href={href}>
+            View project
+          </BlackBtn>
+        </div>
+      </div>
+    </Section>
+  )
+}
+
+export default Project
