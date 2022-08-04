c4cc6018248c1e8f651bef97192bc83a7dcea356
Thanasak <bestzacap20@gmail.com>
2022-08-04 10:00:31 +0700

    add first project component

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,28 @@
+import { Title } from '../components/title'
+import Article from '../components/layout/article'
+import Project from '../components/projects-item'
+import Section from '../components/section'
+
+import weather from '../public/images/projects/weather_eyecatching.png'
+
 const Projects = () => {
-  return <div>This is projects page.</div>
+  return (
+    <Article>
+      <Section>
+        <Title>Some projecsts I&apos;ve done!</Title>
+
+        <Project
+          src={weather}
+          alt={'weather'}
+          title={'Weather Report App'}
+          desc={
+            'A weather report app from OpenWeatherMap API. Support Server Side Rendering, Searchbox Suggestion, and Smooth fade in animation.'
+          }
+          href={'/projects/weather'}
+        />
+      </Section>
+    </Article>
+  )
 }
 
 export default Projects
