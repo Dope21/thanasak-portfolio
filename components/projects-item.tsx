d7c7ae25bf48d4bf681d6123fb484bac22a3e2c8
Thanasak <bestzacap20@gmail.com>
2022-08-05 11:07:03 +0700

    changed text to center on mobile

--- components/projects-item.tsx
+++ components/projects-item.tsx
@@ -1,32 +1,24 @@
 import Image from 'next/image'
-import Section from './section'
 import { BlackBtn } from './button'
 import { AiOutlineRight } from 'react-icons/ai'
 
 const Project = ({ src, alt, title, desc, href }) => {
   return (
-    <Section>
-      <div className="flex flex-col sm:flex-row items-center gap-4 md:gap-10 relative">
-        <div className="max-w-[275px] max-h-[175px] flex items-center justify-center  shadow-lg">
-          <Image
-            src={src}
-            alt={alt}
-            placeholder="blur"
-            className="rounded-md"
-          />
-        </div>
-
-        <div>
-          <h4 className="text-xl main-text-color font-semibold mb-[10px]">
-            {title}
-          </h4>
-          <p className="sub-text-color mb-4">{desc}</p>
-          <BlackBtn icon={<AiOutlineRight size={18} />} href={href}>
-            View project
-          </BlackBtn>
-        </div>
+    <div className="flex flex-col sm:flex-row items-center gap-4 md:gap-10 relative mb-12">
+      <div className="max-w-[275px] max-h-[175px] flex items-center justify-center  shadow-lg">
+        <Image src={src} alt={alt} placeholder="blur" className="rounded-md" />
       </div>
-    </Section>
+
+      <div className="w-full text-center sm:text-left">
+        <h4 className="text-xl main-text-color font-semibold mb-[10px]">
+          {title}
+        </h4>
+        <p className="sub-text-color mb-4">{desc}</p>
+        <BlackBtn icon={<AiOutlineRight size={18} />} href={href}>
+          View project
+        </BlackBtn>
+      </div>
+    </div>
   )
 }
 
