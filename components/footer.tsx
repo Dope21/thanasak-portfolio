3bb669560024bc3acc61d38bdd2d5a12bc244203
Thanasak <bestzacap20@gmail.com>
2022-08-05 11:05:55 +0700

    add padding bottom 1 rem

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="max-w-3xl mx-auto text-center text-zinc-500 text-sm">
+    <footer className="max-w-3xl mx-auto text-center text-zinc-500 text-sm pb-4">
       <h4>Build with Next.js, Tailwind, Framer Motion, and Vercel.</h4>
     </footer>
   )
