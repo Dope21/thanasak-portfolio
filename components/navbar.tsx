9cf2e6576a7414657e40296c3e506d6a2244262b
Thanasak <bestzacap20@gmail.com>
2022-08-07 13:07:11 +0700

    add motion to submenu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import Theme from './theme-button'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { useState } from 'react'
 import Logo from './logo'
+import { motion } from 'framer-motion'
 
 const NavLink = ({ href, children, path }) => {
   const active = path === href
@@ -22,9 +23,18 @@ const NavLink = ({ href, children, path }) => {
 
 const Navbar = ({ path }) => {
   const [menu, setMenu] = useState(false)
+
   const handleSubmenu = () => {
     setMenu(!menu)
   }
+
+  const subMenuAnimate = {
+    close: { scale: 0, opacity: 0, y: -100, x: 100 },
+    open: { scale: 1, opacity: 1, y: 0, x: 0 },
+  }
+
+  const subMenuSpring = { type: 'spring', damping: 25, stiffness: 500 }
+
   return (
     <nav className="fixed w-full backdrop-blur-sm z-10">
       <div className="p-2 flex w-full items-center justify-between max-w-3xl mx-auto">
@@ -51,28 +61,35 @@ const Navbar = ({ path }) => {
             >
               <AiOutlineMenu className="text-zinc-400" />
             </div>
-            <div
-              className={`absolute top-14 right-[9px] w-[220px] h-[165px] drop-shadow-md rounded-md bg-white ${
-                menu ? '' : 'hidden'
-              }`}
+            <motion.div
+              variants={subMenuAnimate}
+              transition={subMenuSpring}
+              animate={menu ? 'open' : 'close'}
+              className="absolute top-14 right-[9px] w-[220px] h-[165px] drop-shadow-md rounded-lg bg-white dark:bg-neutral-800"
             >
-              <div className="h-full flex flex-col text-zinc-500">
+              <div className="h-full flex flex-col text-zinc-500 ">
                 <Link href="/">
-                  <a className="py-2 px-5 hover:text-black">Home</a>
+                  <a className="py-2 px-5 sub-text-color hover:main-text-color">
+                    Home
+                  </a>
                 </Link>
                 <Link href="/projects">
-                  <a className="py-2 px-5 hover:text-black">Projects</a>
+                  <a className="py-2 px-5 sub-text-color hover:main-text-color">
+                    Projects
+                  </a>
                 </Link>
                 <Link href="/skills">
-                  <a className="py-2 px-5 hover:text-black">Skills</a>
+                  <a className="py-2 px-5 sub-text-color hover:main-text-color">
+                    Skills
+                  </a>
                 </Link>
                 <Link href="/contact">
-                  <a className="py-2 px-5 hover:bg-teal-400 dark:bg-red-400">
+                  <a className="py-2 px-5 sub-text-color hover:main-text-color">
                     Contact
                   </a>
                 </Link>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
