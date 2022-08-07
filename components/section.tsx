2449f568318b0692a369b72875c55650a095af0b
Thanasak <bestzacap20@gmail.com>
2022-08-07 13:10:58 +0700

    add motion

--- components/section.tsx
+++ components/section.tsx
@@ -1,5 +1,16 @@
-const Section = ({ children }) => {
-  return <div className="mb-6">{children}</div>
+import { motion } from 'framer-motion'
+
+const Section = ({ children, delay = 0 }) => {
+  return (
+    <motion.div
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.4, delay }}
+      className="mb-6"
+    >
+      {children}
+    </motion.div>
+  )
 }
 
 export default Section
