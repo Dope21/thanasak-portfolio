e50ea4ca84e397350622193a99cdeeda9bec7193
Thanasak <bestzacap20@gmail.com>
2022-08-07 13:18:22 +0700

    add some style and button

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,19 @@
+import { MainBtn } from '../components/button'
+import { AiOutlineHome } from 'react-icons/ai'
+
 const NotFound = () => {
-  return <div>The page you&apos;re looking for was not found.</div>
+  return (
+    <div>
+      <p className="sub-text-color text-center text-lg px-4 py-3 rounded-lg relative border-2">
+        Sorry, the page you&apos;re looking for was not found.
+      </p>
+      <div className="text-center mt-4">
+        <MainBtn href="/" icon={<AiOutlineHome size={18} />}>
+          Back to Home
+        </MainBtn>
+      </div>
+    </div>
+  )
 }
 
 export default NotFound
