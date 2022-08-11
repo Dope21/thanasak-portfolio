53588374cdf7c71093b975101bb330622f21cd0a
Thanasak <bestzacap20@gmail.com>
2022-08-11 21:30:04 +0700

    icons  update & add grid-cols-3 to icons wrapper

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -3,11 +3,9 @@ import Section from '../components/section'
 import { Heading, Title } from '../components/title'
 import Image from 'next/image'
 
-import ReactLogo from '../public/images/skills/react.png'
 import TsLogo from '../public/images/skills/ts.png'
 import NextLogo from '../public/images/skills/next.png'
 import TailwindLogo from '../public/images/skills/tailwind.png'
-import EmotionLogo from '../public/images/skills/emotion.png'
 import ChakraLogo from '../public/images/skills/chakra.png'
 import B5Logo from '../public/images/skills/b5.png'
 import SassLogo from '../public/images/skills/sass.png'
@@ -39,12 +37,10 @@ const Skills = () => {
         <div className="mt-10">
           <Section delay={0.2}>
             <Heading>Front-End</Heading>
-            <div className="grid grid-cols-5 gap-4 items-center justify-center">
+            <div className="grid grid-cols-3 sm:grid-cols-5 gap-4 items-center justify-center">
               <Icon icon={TsLogo}>TypeScript</Icon>
-              <Icon icon={ReactLogo}>React.js</Icon>
               <Icon icon={NextLogo}>Next.js</Icon>
               <Icon icon={TailwindLogo}>Tailwind</Icon>
-              <Icon icon={EmotionLogo}>Emotion</Icon>
               <Icon icon={ChakraLogo}>Chakra UI</Icon>
               <Icon icon={B5Logo}>Bootstrap 5</Icon>
               <Icon icon={SassLogo}>Sass/SCSS</Icon>
@@ -54,7 +50,7 @@ const Skills = () => {
 
           <Section delay={0.3}>
             <Heading>Back-End</Heading>
-            <div className="grid grid-cols-5 gap-4 items-center justify-center">
+            <div className="grid grid-cols-3 sm:grid-cols-5 gap-4 items-center justify-center">
               <Icon icon={NodeLogo}>Node.js</Icon>
               <Icon icon={PhpLogo}>PHP</Icon>
               <Icon icon={MysqlLogo}>MySQL</Icon>
@@ -63,7 +59,7 @@ const Skills = () => {
 
           <Section delay={0.4}>
             <Heading>Other</Heading>
-            <div className="grid grid-cols-5 gap-4 items-center justify-center">
+            <div className="grid grid-cols-3 sm:grid-cols-5 gap-4 items-center justify-center">
               <Icon icon={GitLogo}>Git</Icon>
               <Icon icon={FigmaLogo}>Figma</Icon>
               <Icon icon={PsLogo}>Photoshop</Icon>
