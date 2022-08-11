1735e2946a5f5e5b9f4041cf59d7886d1d96fdc5
Thanasak <bestzacap20@gmail.com>
2022-08-11 22:22:37 +0700

    set dark mode on html tag instead of body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head />
-        <body className="light">
+        <body>
           <Main />
           <NextScript />
         </body>
