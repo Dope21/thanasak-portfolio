6fab407fccba9c5a0db8a9b3960c1b4291fb5289
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-02-26 23:25:38 +0700

    changed to copyright

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="max-w-3xl mx-auto text-center text-zinc-500 text-sm pb-4">
-      <h4>Build with Next.js, Tailwind, Framer Motion, and Vercel.</h4>
+      <h4>© 2023 Thanasak Limsila. All Rights Reserved.</h4>
     </footer>
   )
 }
